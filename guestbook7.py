a7c1a2d1985dbe99fdeb9cb428389cbb5f53bd3e
Your Name <you@example.com>
2022-10-05 20:34:57 +0700

    update

--- guestbook7.py
+++ guestbook7.py
@@ -33,6 +33,6 @@ def process():
     db.session.commit() 
     return redirect(url_for('index'))
     
-if __name__ =='__main__': 
-    app.run(host='0.0.0.0',port=88)
+if __name__ =='__main__':  
+    app.run(debug = True , host='0.0.0.0',port=88)
 
