b2c370ca37b8e0548982656ce937ad803c37ba35
Your Name <you@example.com>
2022-10-18 21:58:29 +0700

    update

--- connected_x.py
+++ connected_x.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def index():
     return render_template('homework-h.html')
 if __name__  == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=80,debug=True)
