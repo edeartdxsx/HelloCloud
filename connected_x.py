ed23c120615e99254c48566166549678d4ecee3b
Your Name <you@example.com>
2022-10-18 21:57:47 +0700

    update

--- connected_x.py
+++ connected_x.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def index():
     return render_template('homework-h.html')
 if __name__  == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(debug=True)
